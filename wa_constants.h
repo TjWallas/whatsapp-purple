81c7285f79148984af8c4bf0de228b9b117f152c
jakibaki <jakibaki@live.com>
2017-11-14 16:37:55 +0100

    Update env to the one loqui im uses

--- wa_constants.h
+++ wa_constants.h
@@ -3,9 +3,8 @@
 
 #define WHATSAPP_SERVER          "s.whatsapp.net"
 
-#define WHATSAPP_VERSION         "S40-2.16.11"
-#define WHATSAPP_USER_AGENT      "User-Agent: WhatsApp/2.16.11 S40/14.26 Device/Nokia-302\r\n"
-
+#define WHATSAPP_VERSION         "S40-2.16.12"
+#define WHATSAPP_USER_AGENT      "User-Agent: WhatsApp/2.16.12 S40Version/14.26 Device/Nokia302\r\n"
 #define WHATSAPP_STATUS_ONLINE   "online"
 #define WHATSAPP_STATUS_AWAY     "away"
 #define WHATSAPP_STATUS_OFFLINE  "offline"
